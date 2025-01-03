b64d3b825dafcc8ed649aed05556b46ed636d187
최혜진 <choihyejin94@naver.com>
2025-01-03 15:06:18 +0900

    유효성검사 함수로 변경 및 스타일링 변경

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -4,7 +4,6 @@ import supabase from '../utils/supabase';
 
 export default function Signup() {
   const navigate = useNavigate();
-  const [user, setUser] = useState(null);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [passwordConfirm, setPasswordConfirm] = useState('');
@@ -12,13 +11,25 @@ export default function Signup() {
 
   const signUpNewUser = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const emailVaildCheck = () => {
-      const pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-      return pattern.test(email);
-    };
-    if (!emailVaildCheck()) {
-      alert('이메일 형식이 맞지 않습니다');
+
+    if (!nickName) {
+      alert('닉네임을 입력해주세요 !');
       return;
+    }
+
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailRegex.test(email)) {
+      alert('이메일 형식이 올바르지 않습니다 !');
+      return;
+    }
+
+    if (password.length < 8) {
+      alert('비밀번호는 8자 이상이어야 합니다 !');
+      return;
+    }
+
+    if (password !== passwordConfirm) {
+      alert('비밀번호가 일치하지 않습니다 !');
     }
 
     const { data, error } = await supabase.auth.signUp({
@@ -32,13 +43,12 @@ export default function Signup() {
     });
     if (error) {
       console.error('supabase auth error', error);
-    } else {
-      setUser(data.user);
-    }
+      return;
+    } 
 
     const { data: loginData, error: loginError } = await supabase
       .from('users')
-      .insert([{ id: data.user.id, email, nickname: nickName }])
+      .insert([{ id: data.user?.id, email, nickname: nickName }])
       .select();
     if (loginError) {
       console.error('supabase insert error', loginError);
@@ -65,44 +75,56 @@ export default function Signup() {
         <h2 className="font-bold text-5xl">회원가입</h2>
         <div className="border-b-2 border-dotted border-black w-[28rem] my-6" />
         <div className="flex flex-col">
-          <input
-            type={'email'}
-            placeholder={'Email'}
-            onChange={(e) => setEmail(e.target.value)}
-            className="border border-black rounded-md p-2 mb-1 text-lg focus:outline-none focus:ring-2 focus:ring-black"
-          />
-          {email ? '' : <p className="text-red-500 text-xs mb-2">이메일을 입력해주세요 !</p>}
-          <input
-            type={'password'}
-            placeholder={'Password'}
-            onChange={(e) => setPassword(e.target.value)}
-            className="border border-black rounded-md p-2 mb-1 text-lg focus:outline-none focus:ring-2 focus:ring-black"
-          />
-          {password.length >= 8 ? '' : <p className="text-red-500 text-xs mb-2">8자 이상 입력해주세요 !</p>}
-          <input
-            type={'password'}
-            placeholder={'Password Confirm'}
-            onChange={(e) => setPasswordConfirm(e.target.value)}
-            className="border border-black rounded-md p-2 mb-1 text-lg focus:outline-none focus:ring-2 focus:ring-black"
-          />
-          {password === passwordConfirm ? (
-            ''
-          ) : (
-            <p className="text-red-500 text-xs mb-2">패스워드가 일치하지 않습니다 !</p>
-          )}
-          <input
-            type={'nickname'}
-            placeholder={'NickName'}
-            onChange={(e) => setNickName(e.target.value)}
-            className="border border-black rounded-md p-2 mb-1 text-lg focus:outline-none focus:ring-2 focus:ring-black"
-          />
-          {nickName ? '' : <p className="text-red-500 text-xs mb-2">닉네임을 입력해주세요 !</p>}
+          <div>
+            <p>Email : </p>
+            <input
+              type={'email'}
+              placeholder={'your@email.com'}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="border border-black rounded-md p-2 mb-3 text-lg w-[14rem] focus:outline-none focus:ring-2 focus:ring-black"
+              required
+            />
+          </div>
+          <div>
+            <p>Password : </p>
+            <input
+              type={'password'}
+              placeholder={'Password'}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="border border-black rounded-md p-2 mb-3 text-lg w-[14rem] focus:outline-none focus:ring-2 focus:ring-black"
+              required
+            />
+          </div>
+          <div>
+            <p>Password Confirm : </p>
+            <input
+              type={'password'}
+              placeholder={'Password'}
+              value={passwordConfirm}
+              onChange={(e) => setPasswordConfirm(e.target.value)}
+              className="border border-black rounded-md p-2 mb-3 text-lg w-[14rem] focus:outline-none focus:ring-2 focus:ring-black"
+              required
+            />
+          </div>
+          <div>
+            <p>NickName : </p>
+            <input
+              type={'nickname'}
+              placeholder={'NickName'}
+              value={nickName}
+              onChange={(e) => setNickName(e.target.value)}
+              className="border border-black rounded-md p-2 mb-3 text-lg w-[14rem] focus:outline-none focus:ring-2 focus:ring-black"
+              required
+            />
+          </div>
         </div>
         <button className="border bg-black text-white rounded p-3 m-2 font-bold text-2xl hover:bg-white hover:text-black hover:border-black cursor-pointer">
           Sign Up
         </button>
         <div>
-          <p className="text-sm mt-2">
+          <p className="mt-2">
             Already have an account?{' '}
             <Link to="/login">
               <button className="text-red-500">Login</button>
