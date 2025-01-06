9bab6cfe92d8dfc2bf8f7fd953134b3adb2e573d
최혜진 <choihyejin94@naver.com>
2025-01-06 09:21:14 +0900

    로그인기능 완성

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,77 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import supabase from "../utils/supabase";
+
 export default function Login() {
-  return <>Login</>;
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const loginUser = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email, password,
+    });
+    if (error) {
+      console.error('supabase login error', error);
+      return;
+    } else {
+      console.log({ data });
+    }
+
+    setEmail('');
+    setPassword('');
+
+    alert('로그인이 완료되었습니다 !');
+
+    navigate('/');
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <form
+        onSubmit={loginUser}
+        className="bg-white border-2 border-black rounded-md flex flex-col justify-center items-center w-[30rem] h-[35rem] shadow-xl"
+      >
+        <h2 className="font-bold text-5xl">로그인</h2>
+        <div className="border-b-2 border-dotted border-black w-[28rem] my-6" />
+        <div className="flex flex-col">
+          <div>
+            <p>Email : </p>
+            <input
+              type={'email'}
+              placeholder={'your@email.com'}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="border border-black rounded-md p-2 mb-3 text-lg w-[14rem] focus:outline-none focus:ring-2 focus:ring-black"
+              required
+            />
+          </div>
+          <div>
+            <p>Password : </p>
+            <input
+              type={'password'}
+              placeholder={'Password'}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="border border-black rounded-md p-2 mb-3 text-lg w-[14rem] focus:outline-none focus:ring-2 focus:ring-black"
+              required
+            />
+          </div>
+        </div>
+        <button className="border bg-black text-white rounded p-3 m-2 font-bold text-2xl hover:bg-white hover:text-black hover:border-black cursor-pointer">
+          Login
+        </button>
+        <div>
+          <p className="mt-2">
+            Don't have an account?{' '}
+            <Link to="/signup">
+              <button className="text-red-500">Sign Up</button>
+            </Link>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
 }
