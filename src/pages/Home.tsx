9b8f707db585d2e1cbe7090b003f419b338278a7
최혜진 <choihyejin94@naver.com>
2025-01-09 11:40:32 +0900

    글 추가 및 수정페이지 UI

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div>
       <div>
         <div className="flex justify-end mb-2">
-          <button className="border bg-black text-white rounded p-2 m-2 text-lg hover:bg-white hover:text-black hover:border-black cursor-pointer">
+          <button className="border bg-black text-white rounded p-2 mr-5 text-lg hover:bg-white hover:text-black hover:border-black cursor-pointer">
             Writing
           </button>
         </div>
